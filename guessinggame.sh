6f4760dd33cee361c11fa0f6f96c9220996e3bfc
Cyber_wolf <51040820+cyberwolf199807@users.noreply.github.com>
2019-05-26 01:45:53 -0500

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+function guessinggame()
+{
+echo "Welcome here mate! please guess the number of files in the current dir"
+read response
+count=$(ls | wc -l)
+while [[ $response -ne $count ]]
+do
+if [[ $response -le 9 ]]
+then
+echo "too high"
+else
+echo "too low"
+fi
+read response
+done
+echo "Congragulations you got it right"
+}
+
